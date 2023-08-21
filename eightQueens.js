4bce97ff508ee378bb8caf81fb9195fafec23a43
Aleksei Lapygin <alex.lapygin@gmail.com>
2023-08-21 21:54:04 +0200

    eight queens refactor

--- eightQueens.js
+++ eightQueens.js
@@ -4,33 +4,28 @@ const eightQueens = () => {
   let i = 0;
   let j = 0;
   while (i < 8) {
-    while (true) {
-      if (j < 8 && result.every((pos) => isNotChecked(pos, [i, j]))) {
-        result.push([i, j]);
-        j = 0;
-        break;
-      }
-      j++;
-      if (j > 7) {
+    while (j < 8 && result.some((pos) => isChecked(pos, [i, j]))) {
+      if (j >= 7) {
         [i, j] = result.pop();
-        j++;
       }
+
+      j++;
     }
+
+    if (j < 8) {
+      result.push([i, j]);
+    }
+
+    j = 0;
     i++;
   }
 
   return result;
 };
 
-const isNotChecked = (pos1, pos2) => {
-  if (
-    pos1[0] === pos2[0] ||
-    pos1[1] === pos2[1] ||
-    Math.abs(pos1[0] - pos2[0]) === Math.abs(pos1[1] - pos2[1])
-  ) {
-    return false;
-  }
-  return true;
-};
+const isChecked = (pos1, pos2) =>
+  pos1[0] === pos2[0] ||
+  pos1[1] === pos2[1] ||
+  Math.abs(pos1[0] - pos2[0]) === Math.abs(pos1[1] - pos2[1]);
 
 console.log(eightQueens());
