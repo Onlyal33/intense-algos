3615b112b5fac321c97502e46992f6e63fad2e77
Aleksei Lapygin <alex.lapygin@gmail.com>
2023-08-21 17:42:22 +0200

    selectionsort

---
+++ selectionsort.js
@@ -0,0 +1,15 @@
+const data = [5, 4, 3, 2, 1];
+
+const solution = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minId = i;
+    for (let j = i; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        minId = j + 1;
+      }
+      [arr[i], arr[minId]] = [arr[minId], arr[i]];
+    }
+  }
+};
+solution(data);
+console.log(data);
